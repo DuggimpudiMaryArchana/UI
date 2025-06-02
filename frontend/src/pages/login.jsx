448cd7b07000c76a038ddf2a04177141a992d85b
DuggimpudiMaryArchana <137522005+DuggimpudiMaryArchana@users.noreply.github.com>
2025-06-02 13:15:40 +0530

    Create login.jsx

---
+++ frontend/src/pages/login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import LoginForm from '../components/Auth/LoginForm';
+function Login() {
+  return (
+    <div className='d-flex justify-content-center align-items-center vh-100 bg-light'>
+        
+          <LoginForm/>
+        </div>
+        
+  
+  );
+}
+
+export default Login;
