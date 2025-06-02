4f61274c178d26b3f418ebe4bbfeb11e0a2dc45d
DuggimpudiMaryArchana <137522005+DuggimpudiMaryArchana@users.noreply.github.com>
2025-06-02 13:16:06 +0530

    Create Register.jsx

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import RegisterForm from "../components/Auth/RegisterForm";
+
+function Register() {
+    return (
+        <div className='d-flex justify-content-center align-items-center vh-100 bg-light'>
+
+            <RegisterForm />
+        </div>
+
+
+    );
+}
+
+export default Register;
